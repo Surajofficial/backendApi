d035137895f2548df73e872b73311da4489d1f8d
surajofficial <surajofficial694@gmail.com>
2025-11-17 12:56:27 +0530

    made resource api

--- routes/api.php
+++ routes/api.php
@@ -7,27 +7,21 @@ use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\UserProfileController;
 use App\Http\Controllers\Api\Admin\UserController as AdminUserController;
 
-Route::prefix('auth')->group(function () {
-    Route::post('register', [AuthController::class, 'register']);
-    Route::post('login', [AuthController::class, 'login']);
-    Route::get('captcha', [AuthController::class, 'captcha']);
-    Route::middleware('auth:sanctum')->post('logout', [AuthController::class, 'logout']);
-    Route::middleware('auth:sanctum')->get('user', [AuthController::class, 'user']);
-});
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+Route::get('captcha', [AuthController::class, 'captcha']);
 
 // User profile
 Route::middleware('auth:sanctum')->group(function () {
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::get('user', [AuthController::class, 'user']);
     Route::get('user/profile', [UserProfileController::class, 'show']);
     Route::put('user/profile', [UserProfileController::class, 'update']);
 });
 
 // Admin routes
 Route::middleware(['auth:sanctum', 'can:manage-users'])->prefix('admin')->group(function () {
-    Route::get('users', [AdminUserController::class, 'index']);
+    Route::apiResource('users', AdminUserController::class);
     Route::get('users/search', [AdminUserController::class, 'search']);
-    Route::get('users/{user}', [AdminUserController::class, 'show']);
-    Route::post('users', [AdminUserController::class, 'store']);
-    Route::put('users/{user}', [AdminUserController::class, 'update']);
-    Route::delete('users/{user}', [AdminUserController::class, 'destroy']);
 });
 
